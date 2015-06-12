0e740c68ff4d34cb47e4a28cc37f3dea2e0647c8
Will Taylor <will.taylor@dealer.com>
2015-06-12 12:01:19 -0400

    params

--- case62975/src/main/scala/com/dealertrack/Test.scala
+++ case62975/src/main/scala/com/dealertrack/Test.scala
@@ -7,7 +7,7 @@ object Test {
   val defaultPrefix = "/tmp/spark-bug"
 
   def main(args: Array[String]): Unit = {
-    val locationPrefix = args.toSeq match {
+    val locationPrefix = args.toList match {
       case Nil => FileUtils.deleteDirectory(new java.io.File(defaultPrefix)); defaultPrefix
       case prefix :: Nil => prefix
     }
