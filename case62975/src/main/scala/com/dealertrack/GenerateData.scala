a80df1620710669356c2475af737e490c93ff49c
Will Taylor <will.taylor@dealer.com>
2015-06-12 08:38:18 -0400

    progress

--- case62975/src/main/scala/com/dealertrack/GenerateData.scala
+++ case62975/src/main/scala/com/dealertrack/GenerateData.scala
@@ -1,15 +1,16 @@
 package com.dealertrack
 
+import java.util.UUID
+
 import com.dealer.spark.example.DataOne
-import org.apache.avro.Schema
 import org.apache.avro.mapred.AvroKey
 import org.apache.avro.mapreduce.{AvroKeyOutputFormat, AvroJob}
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.io.NullWritable
-import org.apache.hadoop.mapred.{FileInputFormat, JobConf}
 import org.apache.hadoop.mapreduce.Job
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
 import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.SparkContext._
 
 import scala.util.Random
 
@@ -21,14 +22,21 @@ object GenerateData {
     val conf = new SparkConf().setMaster("local[2]").setAppName("Sample Data Generator")
     val sc = new SparkContext(conf)
 
-    val conf = new Job()
+    val randomCharStream = new Random().alphanumeric.grouped(25)
+    def nextId: String = randomCharStream.next().mkString
+
+    val lotsOfData = Seq.fill(50000) {
+      DataOne.newBuilder().setMyId(UUID.randomUUID().toString).setSomeData(random.nextString(10000)).build
+    }
+
+    val job = new Job()
     val schema = DataOne.SCHEMA$
 
-    FileOutputFormat.setOutputPath(conf, new Path("/tmp/spark-bug"))
-    AvroJob.setOutputKeySchema(conf, schema)
-    conf.setOutputFormatClass(classOf[AvroKeyOutputFormat[DataOne]])
+    FileOutputFormat.setOutputPath(job, new Path("/tmp/spark-bug"))
+    AvroJob.setOutputKeySchema(job, schema)
+    job.setOutputFormatClass(classOf[AvroKeyOutputFormat[DataOne]])
 
-    output.good.map(new AvroKey(_) -> NullWritable.get).saveAsNewAPIHadoopDataset(conf.getConfiguration)
+    lotsOfData.map(new AvroKey(_) -> NullWritable.get).saveAsNewAPIHadoopDataset(job.getConfiguration)
 
   }
 
