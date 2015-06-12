9e5efbda3e6b97500cc7f87cb984ee1035e9eeec
Joshua.Dickerson <Joshua.Dickerson@dealer.com>
2015-06-12 08:39:54 -0400

    using generated path

--- case62975/src/main/scala/com/dealertrack/ErrorExample.scala
+++ case62975/src/main/scala/com/dealertrack/ErrorExample.scala
@@ -11,13 +11,14 @@ import org.apache.spark.SparkContext
  */
 object ErrorExample {
   def main(args: Array[String]): Unit ={
+
     val conf = new SparkConf().setMaster("local[2]").setAppName("ImpressionAggregator")
     val sc = new SparkContext(conf)
     //    val source = getClass.getResource("/2015-03-24-18--vtdevana-cloudera14.dealer.ddc-RTBParsedImpression.1427222183676.avro").getPath
 
     // this hdfs instance may not be the master, if you see an error to that effect,
     // look at the hdfs cluster here: http://vtdevana-cloudera10.dealer.ddc:7180/cmf/services/37/status
-    val source = "hdfs://vtqaana-cloudera01.dealer.ddc:8020/ad/RTBImpression/incoming/2015/04/27/13/"
+    val source = "/tmp/spark-bug"
 
     val job = new JobConf()
     job.set("mapreduce.input.fileinputformat.input.dir.recursive", "true")
